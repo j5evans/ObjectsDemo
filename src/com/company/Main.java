5198e97b0ffc03de39a7de4cc5f66db186b06e08
j5evans <j5evans@student.bridgew.edu>
2021-09-27 14:46:44 -0400

    added Customer and Bank class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,17 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        var myAccount = new BankAccount();
-        var yourAccount = new BankAccount(2000, 0.05f);
-        myAccount.deposit(1000);
-        myAccount.addInterest();
-        yourAccount.addInterest();
-        System.out.println("Your account (ID: " + yourAccount.getAccountID() + ") currently has a balance of " +
-                yourAccount.checkBalance());
-        var didSucceed = myAccount.withdraw(2000);
-        if (didSucceed)
-            System.out.println("Successfully made withdrawal.");
-        else
-            System.out.println("Could not withdraw, current balance (ID: " + myAccount.getAccountID() + ") is " + myAccount.checkBalance());
+        var comp152Bank = new Bank();
+        comp152Bank.doBanking();
     }
 }
